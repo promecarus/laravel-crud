cf8f880089f6751e9dba6fbfa307205bda7f26ef
promecarus <haikalslipi@gmail.com>
2023-06-09 20:56:37 +0700

    add vite

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta content="ie=edge" http-equiv="X-UA-Compatible">
+	<title>{{ $title ?? 'amonus' }}</title>
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    
+</body>
+
+</html>
